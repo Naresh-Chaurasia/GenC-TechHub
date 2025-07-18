f09d46d4f02244eb0c6bbce6020559bf182e9cb4
Naresh Chaurasia <johndoe@example.com>
2025-07-18 10:53:30 +0530

    Macbook Pro, Checkin Timestamp::Fri Jul 18 10:53:30 IST 2025

--- GenC-SpringFramework1/src/main/java/com/sorting1/ISort.java
+++ GenC-SpringFramework1/src/main/java/com/sorting1/ISort.java
@@ -1,4 +1,4 @@
-package com.sorting;
+package com.sorting1;
 
 public interface ISort {
     void sort(int[] numbers);
--- GenC-SpringFramework1/src/main/java/com/sorting1/MainSort.java
+++ GenC-SpringFramework1/src/main/java/com/sorting1/MainSort.java
@@ -1,4 +1,4 @@
-package com.sorting;
+package com.sorting1;
 
 public class MainSort {
 
