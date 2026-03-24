5dfc513e11decf66bd824348cddf8cc0033e852b
Naresh Chaurasia <johndoe@example.com>
2026-03-24 15:58:51 +0530

    Macbook Pro, Checkin Timestamp::Tue Mar 24 15:58:51 IST 2026

--- GenC-Design-Patterns/src/com/dp/singleton/SingletonConcept.java
+++ GenC-Design-Patterns/src/com/dp/singleton/SingletonConcept.java
@@ -1,22 +1,36 @@
 package com.dp.singleton;
 
 class Bird{
+
+    private Bird() {
+        System.out.println("Bird constructor called");
+    }
+    
     public void fly(){
         System.out.println("Bird is flying");
+    }
+    
+    public static Bird getInstance() {
+        return new Bird();
     }
 }
 
 public class SingletonConcept {
 
     public static void main(String[] args) {
-        Bird bird = new Bird();
-        bird.fly();
-        System.out.println("Bird 1: " + bird);
-        System.out.println("Bird 1: " + bird);
 
-        Bird bird2 = new Bird();
-        bird2.fly();
-        System.out.println("Bird 2: " + bird2);
+
+       Bird b =  Bird.getInstance();
+       b.fly();
+
+               // Bird bird = new Bird();
+        // bird.fly();
+        // System.out.println("Bird 1: " + bird);
+        // System.out.println("Bird 1: " + bird);
+
+        // Bird bird2 = new Bird();
+        // bird2.fly();
+        // System.out.println("Bird 2: " + bird2);
     }
 
 }
