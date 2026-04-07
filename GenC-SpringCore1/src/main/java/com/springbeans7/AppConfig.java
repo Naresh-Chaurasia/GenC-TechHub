0d75b470190afdd7c311b4f7c3fee760ea6a5d7c
Naresh Chaurasia <johndoe@example.com>
2026-04-07 14:54:19 +0530

    Macbook Pro, Checkin Timestamp::Tue Apr  7 14:54:19 IST 2026

--- GenC-SpringCore1/src/main/java/com/springbeans7/AppConfig.java
+++ GenC-SpringCore1/src/main/java/com/springbeans7/AppConfig.java
@@ -2,9 +2,14 @@ package com.springbeans7;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import javax.annotation.PostConstruct;
 
 @Configuration
-@ComponentScan(basePackages = "com.springbeans3")
+@ComponentScan(basePackages = "com.springbeans7")
 public class AppConfig {
 
+    @PostConstruct
+    public void onStartup() {
+        System.out.println("Spring context initialized - @PostConstruct processing enabled");
+    }
 }
--- GenC-SpringCore1/src/main/java/com/springbeans7/LifecycleBean.java
+++ GenC-SpringCore1/src/main/java/com/springbeans7/LifecycleBean.java
@@ -1,8 +1,8 @@
 package com.springbeans7;
 
 import org.springframework.stereotype.Component;
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @Component
 public class LifecycleBean {
